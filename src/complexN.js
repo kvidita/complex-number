dddbd8b3aa16540f1dc63bb0c149db331fd030ff
Vidita Kumari <kvidita@users.noreply.github.com>
2023-05-03 17:02:36 +0530

    added multiply and display function for complex numbers

--- src/complexN.js
+++ src/complexN.js
@@ -8,12 +8,41 @@ const complexNumber = function(real, img) {
   }
 
   const add = function(num) {
-    addedReal = num.getReal() + real;
-    addedImg = num.getImg() + img;
+    const addedReal = num.getReal() + real;
+    const addedImg = num.getImg() + img;
+
     return complexNumber(addedReal, addedImg);
   }
 
-  return {getReal, getImg, add};
+  const multiply = function(num) {
+    const a = real;
+    const b = img;
+    const c = num.getReal();
+    const d = num.getImg();
+
+    const productReal = a * c - b * d;
+    const productImg = a * d + b * c;
+
+    return complexNumber(productReal, productImg);
+  }
+
+  const displayComplexNum = function() {
+    if(real === 0) {
+      return img + 'i';
+    }
+
+    if(img === 0) {
+      return real.toString();
+    }
+
+    if(img < 0) {
+      return real + ' - ' + -1 * img + 'i';
+    }
+
+    return real + ' + ' + img + 'i';
+  }
+
+  return {getReal, getImg, add, multiply, displayComplexNum};
 }
 
-const a = complexNumber()
+exports.complexNumber = complexNumber;
